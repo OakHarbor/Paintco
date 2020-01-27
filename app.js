42b76304f4944e3dd764266a08d9dfd9834f05db
Postal06 <ohwdinfo@gmail.com>
2020-01-27 02:50:44 -0800

    added portfolio page and auto prefixed

--- app.js
+++ app.js
@@ -42,3 +42,99 @@ let toggleNav = function () {
         toggleNavStatus = false;
     }
 }
+
+/* Modals collected in variables */
+
+var $modalHome = $(".modal-home");
+var $modalNewHome = $(".modal-new-home");
+var $modalRoom = $(".modal-room");
+var $modalStairs = $(".modal-stairs");
+var $modalVictorian2 = $(".modal-victorian-2");
+var $modalVictorian4 = $(".modal-victorian-4");
+var $modalVictorian = $(".modal-victorian");
+var $modalWhite = $(".modal-white");
+var $modalVictorian3 = $(".modal-victorian-3");
+var $modalRoom3 = $(".modal-room3");
+
+/* Images collected in variables */
+
+var $homeIcon = $(".home-icon");
+var $html = $("html");
+var $homec = $(".home");
+var $newHome = $("#new-homec");
+var $room = $("#roomc");
+var $stairs = $("#stairsc");
+var $victorian2 = $("#victorian-2c");
+var $victorian4 = $("#victorian-4c");
+var $victorian = $("#victorianc");
+var $white = $("#whitec");
+var $victorian3 = $("#victorian-3c");
+var $room3 = $("#room3c");
+
+/* Add Clicked Class */
+
+$homec.on("click", function(e) {
+  $modalHome.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$newHome.on("click", function(e) {
+  $modalNewHome.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$room.on("click", function(e) {
+  $modalRoom.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$stairs.on("click", function(e) {
+  $modalStairs.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$victorian2.on("click", function(e) {
+  $modalVictorian2.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$victorian4.on("click", function(e) {
+  $modalVictorian4.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$victorian.on("click", function(e) {
+  $modalVictorian.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$white.on("click", function(e) {
+  $modalWhite.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$victorian3.on("click", function(e) {
+  $modalVictorian3.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$room3.on("click", function(e) {
+  $modalRoom3.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+/* Remove Clicked Class */
+
+$homeIcon.on("click", function(e) {
+  $modalHome.removeClass("clicked");
+  $modalNewHome.removeClass("clicked");
+  $modalRoom.removeClass("clicked");
+  $modalStairs.removeClass("clicked");
+  $modalVictorian2.removeClass("clicked");
+  $modalVictorian4.removeClass("clicked");
+  $modalVictorian.removeClass("clicked");
+  $modalWhite.removeClass("clicked");
+  $modalVictorian3.removeClass("clicked");
+  $modalRoom3.removeClass("clicked");
+  $html.removeClass("clicked");
+});
